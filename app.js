53ba125f56ec24ddc446fed38cbae6bb32958cd1
linux-top <540926960@qq.com>
2015-04-26 07:42:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
  * Created by linux-top on 15-4-9.
  */
 
+    //[编辑]
+    //$("*").on("click","",function(){
+    //    //双击变成可编辑
+    //   $(".node").removeAttr("contentEditable");
+    //});
+
     //[标识]当前
     $("*").on("click",".node",function(){
         //任何node-DIV被单击都会变成活动元素:
@@ -10,46 +16,33 @@
         $(this).addClass("active");
         $(this).attr("contentEditable",true);
     });
-
     //[可拖动]
     $("*").on("mouseover",".node",function(){
         //鼠标所到之处,div都变的"可拖动";
         $(this).draggable();
     });
-
-    //[编辑]
-    //$("*").on("click","",function(){
-    //    //双击变成可编辑
-    //   $(".node").removeAttr("contentEditable");
-    //});
-
     //[正在拖动...]
     $("*").on("mousemove","div",function(){
         //曲线动画;
-        //console.log("div[id="+$(this).attr("father")+"]");
-		//console.log("div[id="+$(this).attr("id")+"]");
-		//console.log(this);
         $("path[id="+$(this).attr("id")+"]").attr("d",flushPath("div[id="+$(this).attr("father")+"]",this,0.6));
-		$("path[father="+$(this).attr("id")+"]").attr("d",flushPath(this,"div[id="+$("path[father="+$(this).attr("id")+"]").attr("id")+"]",0.6));
-		//-------------------------------------------------------------此处有问题，静态方法不能处理多个对象（曲线），要写成原型方法；；；；；
+        var _this=this;
+        $("path[father="+$(this).attr("id")+"]").each(function(){
+            console.log($(this));
+            $(this).attr("d",flushPath(_this,"div[id="+$(this).attr("id")+"]",0.6));
+        });
     });
 
 $(document).ready(function(){
     var _id=1;//每个节点[div]、曲线[svg path]的ID产生器;
-    //var _cId=0;//每个曲线的ID产生器;
-
-    //[添加结点]
     $("#addNode").button().on("click",function(){
+        //[添加结点]
         //console.log("正在为活动元素添加内容...");
-
         //1:给当前元素添加一个ID为"_id"的<div>;
         $(".active").createNodeObj(++_id);
         //2:为其生成曲线;
         createCurve(".active","#"+_id,0.6,_id);
-
     });
 });
-
     function createCurve(_filter1,_filter2,_k,_cId){
         //path的html母体结构修改：
         _pathStr="<svg><path id="+_cId+" d='"+flushPath(_filter1,_filter2,_k)+"' father='"+$(_filter1).attr("id")+"' stroke='red' fill='none' style='stroke-width: 2px'></path></svg>";
